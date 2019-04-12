1c0b0c67c7baed32e7f5e1c28ef51ca1b95d11f0
Luvo Ntshewula <luvo@hepstar.local>
2019-04-12 10:04:47 +0200

    added a main method

--- DriverGenrerSize.java
+++ DriverGenrerSize.java
@@ -15,4 +15,10 @@ public class DriverGenrerSize{
 
 		return resultString;
 	}
+	
+	public static void main(String[] args) {
+		DriverGenrersSize gs = new DriverGenrersSize();
+		String s = "my.song.mp3 11b" + '\n' + "greatSong.flac 1000b" + '\n' + "not3.txt 5b" + '\n' + "video.mp4 200b" + '\n' + "game.exe 100b" + '\n' + "mov!e.mkv 10000b";
+		System.out.print(gs.solution(s));
+	}
 }
